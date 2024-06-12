b9a56e13f45fe73c10a3db034a59f0104ac820a9
elisa <5738239+elisav@users.noreply.github.com>
2024-06-12 20:18:22 +1000

    Improved detection

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -5,62 +5,57 @@ import cv2
 import os
 import pathlib
 
-LOWER_WHITE = [215, 215, 215]
-UPPER_WHITE = [255, 255, 255]
-NEON_GREEN = [0, 255, 127]
+LOWER_WHITE = np.array([200, 200, 200])
+UPPER_WHITE = np.array([255, 255, 255])
+NEON_GREEN = np.array([0, 255, 127])
+PAD_RATIO = 0.1
+MIN_OBJECT_RATIO = 0.1
 
 
-def get_split_images(img):
-    height, width, _ = img.shape
-    pad_height = int(height * 0.1)
-    pad_width = int(width * 0.1)
-    img = cv2.copyMakeBorder(img, pad_height, pad_height, pad_width,
-                             pad_width, cv2.BORDER_CONSTANT, value=UPPER_WHITE)
-    contours, _ = get_mask(img)
-
-    rectangles = []
-    for cnt in contours:
-        rect = cv2.minAreaRect(cnt)
-        box = cv2.boxPoints(rect)
-        box = np.int0(box)
-        x, y, w, h = cv2.boundingRect(box)
-        if w >= width * 0.1 and h >= height * 0.1:  # filter out small objects
-            rectangles.append(cnt)
-
-    rectangle_coordinates = []
-    for _, rect in enumerate(rectangles):
-        rect2 = cv2.minAreaRect(rect)
-        box = cv2.boxPoints(rect2)
-        box = np.int0(box)
-        x, y, w, h = cv2.boundingRect(box)
-        x1 = x - pad_width
-        y1 = y - pad_height
-        x2 = x1 + w
-        y2 = y1 + h
-        rectangle_coordinates.append((y1, y2, x1, x2))
-    return rectangle_coordinates
+def get_bounding_box(cnt):
+    rect = cv2.minAreaRect(cnt)
+    box = cv2.boxPoints(rect)
+    return cv2.boundingRect(np.int0(box))
 
 
 def get_mask(img):
-    lower_white = np.array(LOWER_WHITE)
-    upper_white = np.array(UPPER_WHITE)
-    mask = cv2.inRange(img, lower_white, upper_white)
+    mask = cv2.inRange(img, LOWER_WHITE, UPPER_WHITE)
     mask = cv2.bitwise_not(mask)
     contours, _ = cv2.findContours(
         mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    min_width = 0 * img.shape[1]
-
-    filtered_contours = []
-    for cnt in contours:
-        _, _, w, _ = cv2.boundingRect(cnt)
-        if w >= min_width:
-            filtered_contours.append(cnt)
-
+    min_width = MIN_OBJECT_RATIO * img.shape[1]
+    filtered_contours = [cnt for cnt in contours if cv2.boundingRect(cnt)[
+        2] >= min_width]
     mask = np.zeros_like(mask)
     cv2.drawContours(mask, filtered_contours, -1, (255), thickness=cv2.FILLED)
     mask = cv2.bitwise_not(mask)
     return filtered_contours, mask
+
+
+def add_border(img, pad_height, pad_width):
+    return cv2.copyMakeBorder(img, pad_height, pad_height, pad_width, pad_width, cv2.BORDER_CONSTANT, value=[255, 255, 255])
+
+
+def create_rectangle(cnt):
+    rect = cv2.minAreaRect(cnt)
+    box = cv2.boxPoints(rect)
+    return cv2.boundingRect(np.int0(box))
+
+
+def get_split_images(img):
+    height, width, _ = img.shape
+    pad_height = int(height * PAD_RATIO)
+    pad_width = int(width * PAD_RATIO)
+    img = add_border(img, pad_height, pad_width)
+    contours, _ = get_mask(img)
+
+    rectangles = [cnt for cnt in contours if create_rectangle(
+        cnt)[2] >= width * MIN_OBJECT_RATIO and create_rectangle(cnt)[3] >= height * MIN_OBJECT_RATIO]
+
+    rectangle_coordinates = [(y - pad_height, y - pad_height + h, x - pad_width,
+                              x - pad_width + w) for x, y, w, h in map(create_rectangle, rectangles)]
+
+    return rectangle_coordinates
 
 
 def get_rotation_angle(coordinates, image_width, image_height):
@@ -102,47 +97,44 @@ def rotate_image(image, coordinates):
     rotation_mat[1, 2] += bound_h/2 - image_center[1]
 
     rotated_image = cv2.warpAffine(
-        image, rotation_mat, (bound_w, bound_h), borderValue=(UPPER_WHITE))
+        image, rotation_mat, (bound_w, bound_h), borderValue=([255, 255, 255]))
     return rotated_image
 
 
 class ImageProcessor:
     def __init__(self, input_folder, output_folder):
-        self.input_folder = input_folder
-        self.output_folder = output_folder
+        self.input_folder = pathlib.Path(input_folder)
+        self.output_folder = pathlib.Path(output_folder)
         self.initialize_folders()
 
     def initialize_folders(self):
-        if not os.path.exists(self.input_folder):
-            os.makedirs(self.input_folder)
-        if not os.path.exists(self.output_folder):
-            os.makedirs(self.output_folder)
+        self.input_folder.mkdir(parents=True, exist_ok=True)
+        self.output_folder.mkdir(parents=True, exist_ok=True)
 
     def process_images(self):
-        pic_list = list(pathlib.Path(self.input_folder).glob('*.tif'))
-        for pic_index in range(len(pic_list)):
-            img = cv2.imread(str(pic_list[pic_index]))
+        pic_list = list(self.input_folder.glob('*.tif'))
+        for pic_index, pic in enumerate(pic_list):
+            img = cv2.imread(str(pic))
             image_coordinates = get_split_images(img)
-            print(
-                f'Working on {pic_list[pic_index]}: {pic_index+1} of {len(pic_list)}')
-            img = matplotlib.image.imread(pic_list[pic_index])
+            print(f'Working on {pic}: {pic_index+1} of {len(pic_list)}')
+            img = matplotlib.image.imread(pic)
 
             # Loop through images
-            for index in range(len(image_coordinates)):
-               # try:
-                print(f'Working on {index+1} from {pic_list[pic_index]}')
-                y1, y2, x1, x2 = image_coordinates[index]
-                curr_img = img[y1:y2, x1:x2]
-                curr_img = cv2.cvtColor(
-                    curr_img, cv2.COLOR_BGR2RGB)
-                _, mask = get_mask(img)
-                # img[mask == 255] = [0, 255, 127]
-                # mask = cv2.bitwise_not(mask)
-                curr_img = rotate_image(
-                    curr_img, image_coordinates[index])
-                cv2.imwrite(os.path.join(self.output_folder, os.path.basename(
-                    pic_list[pic_index]) + '_' + str(index+1) + '.png'), curr_img)
-               # except Exception as e:
-                #    print(
-                #        f'Error with {index + 1} from {pic_list[pic_index]} : { e }')
-                #    continue
+            for index, coordinates in enumerate(image_coordinates):
+                try:
+                    print(f'Working on {index+1} from {pic}')
+                    y1, y2, x1, x2 = coordinates
+                    curr_img = cv2.cvtColor(
+                        img[y1:y2, x1:x2], cv2.COLOR_BGR2RGB)
+                    filtered_contours, _ = get_mask(curr_img)
+                    # for contour in filtered_contours:
+                    #     x, y, w, h = cv2.boundingRect(contour)
+                    #     cv2.rectangle(curr_img, (x, y),
+                    #                   (x + w, y + h), (0, 255, 0), 10)
+                    # curr_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+                    cv2.imwrite(str(self.output_folder /
+                                f'{pic.stem}_{index+1}.png'), curr_img)
+                except Exception as e:
+                    print(f'Error with {index + 1} from {pic} : { e }')
+                    continue
