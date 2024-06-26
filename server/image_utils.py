1a96f8e982c22a8df280cae4a7735e42b0b42f2b
elisa <5738239+elisav@users.noreply.github.com>
2024-06-27 00:34:26 +1000

    Rotation improvements

--- server/image_utils.py
+++ server/image_utils.py
@@ -3,7 +3,7 @@ import numpy as np
 import base64
 from scipy.optimize import minimize
 
-CORNER_THRESHOLD = 200
+CORNER_THRESHOLD = 220
 WHITE_COLOR = (255, 255, 255)
 PAD_RATIO = 0.01
 MIN_OBJECT_RATIO = 0.1
@@ -25,15 +25,12 @@ def add_border_to_image(img, pad_height, pad_width):
 def extract_subimage_by_coordinates(image, coordinates):
     y1, y2, x1, x2 = coordinates
     subimage = cv2.cvtColor(image[y1:y2, x1:x2], cv2.COLOR_BGR2RGB)
-    subimage = crop_image(subimage)
     rotated_subimage = rotate_image(subimage)
 
     # subtract 1% from each side to remove border
-    height, width, _ = rotated_subimage.shape
-    # border = int(
-    #    min(rotated_subimage.shape[0], rotated_subimage.shape[1]) * 0.002)
-   # rotated_subimage = rotated_subimage[border:height -
-   #                                    border, border:width - border]
+    # height, width, _ = rotated_subimage.shape
+    # border = int(min(rotated_subimage.shape[0], rotated_subimage.shape[1]) * 0.002)
+    # rotated_subimage = rotated_subimage[border:height - border, border:width - border]
     return rotated_subimage
 
 
@@ -88,75 +85,76 @@ def generate_thumbnail(image):
 
 
 def rotate_image(img):
-    img = crop_image(img)
     mask = get_mask(img, CORNER_THRESHOLD, 255)
     angle = optimal_rotation(mask)
     M = cv2.getRotationMatrix2D(
         (img.shape[1] / 2, img.shape[0] / 2), angle, 1)
     img = cv2.warpAffine(
         img, M, (img.shape[1], img.shape[0]), borderValue=(255, 255, 255))
-    img = crop_image(img)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = crop_image(img)
     return img
+
+
+def pixel_diff(angle, mask):
+    M = cv2.getRotationMatrix2D(
+        (mask.shape[1] / 2, mask.shape[0] / 2), angle[0], 1)
+    rotated_mask = cv2.warpAffine(
+        mask, M, (mask.shape[1], mask.shape[0]), borderValue=(255, 255, 255))
+
+    white_threshold = 0.9
+
+    allwhiterowcount = sum(
+        [1 for row in rotated_mask if np.mean(row == 255) >= white_threshold])
+    allwhitecolcount = sum(
+        [1 for col in rotated_mask.T if np.mean(col == 255) >= white_threshold])
+    maxerror = rotated_mask.shape[0] + rotated_mask.shape[1]
+    loss = 1 - (allwhiterowcount + allwhitecolcount) / maxerror
+    print('Loss', loss)
+    return loss
 
 
 def optimal_rotation(mask):
     starting_angle = get_starting_angle(mask)
     result = minimize(pixel_diff, [starting_angle], args=(mask,), method='L-BFGS-B', bounds=[(-45, 45)],
                       options={'finite_diff_rel_step': starting_angle, 'eps': starting_angle / 2})
+    print('Result', result.x[0])
     return result.x[0] if result.success else 0
 
 
-def pixel_diff(angle, img):
-    try:
-        M = cv2.getRotationMatrix2D(
-            (img.shape[1] / 2, img.shape[0] / 2), angle[0], 1)
-        rotated_img = cv2.warpAffine(
-            img, M, (img.shape[1], img.shape[0]), borderValue=(255, 255, 255))
-
-        pixel_difference = np.sum(img == 0) - np.sum(rotated_img == 0)
-        if pixel_difference > 0:
-            return 1e4 * pixel_difference
-
-        rotated_img = crop_image(rotated_img)
-        nonpixel_difference = np.sum(
-            img == 255) - np.sum(rotated_img == 255)
-
-        ratio = nonpixel_difference / \
-            (pixel_difference if pixel_difference != 0 else 1)
-        return ratio
-    except Exception as e:
-        print(f"Error in pixel_diff: {e}")
-        return 1e9
-
-
 def get_starting_angle(mask):
-    POS_ROTATION = 0.5
+    POS_ROTATION = 0.25
     NEG_ROTATION = POS_ROTATION * -1
     neg_rotation_result = pixel_diff([NEG_ROTATION], mask)
     pos_rotation_result = pixel_diff([POS_ROTATION], mask)
-    rotation_total = neg_rotation_result + pos_rotation_result
     if neg_rotation_result < pos_rotation_result:
-        print('neg', (neg_rotation_result / rotation_total) * POS_ROTATION - 1)
-        return (neg_rotation_result / rotation_total) * POS_ROTATION - 1
+        return (1 - neg_rotation_result) * -3
     else:
-        print('pos', (pos_rotation_result / rotation_total) * NEG_ROTATION - 1)
-        return (pos_rotation_result / rotation_total) * NEG_ROTATION + 1
+        return (1 - pos_rotation_result) * 3
 
 
-def get_mask(img, lower_threshold, upper_threshold):
-    lower_threshold = np.array([lower_threshold] * 3)
-    upper_threshold = np.array([upper_threshold] * 3)
+def get_mask(img, mask_1, mask_2):
+    lower_threshold = np.array([mask_1] * 3)
+    upper_threshold = np.array([mask_2] * 3)
     mask = cv2.inRange(img, lower_threshold, upper_threshold)
     mask = cv2.bitwise_not(mask)
     contours, _ = cv2.findContours(
         mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     min_width = MIN_OBJECT_RATIO * img.shape[1]
+
+    # Filter contours based on width
     filtered_contours = [cnt for cnt in contours if cv2.boundingRect(cnt)[
         2] >= min_width]
-    mask = np.zeros_like(mask)
-    cv2.drawContours(mask, filtered_contours, -1,
-                     255, thickness=cv2.FILLED)
+
+    # Find the largest contour based on area
+    if filtered_contours:
+        largest_contour = max(filtered_contours, key=cv2.contourArea)
+        mask = np.zeros_like(mask)
+        cv2.drawContours(mask, [largest_contour], -
+                         1, 255, thickness=cv2.FILLED)
+    else:
+        mask = np.zeros_like(mask)
+
     mask = cv2.bitwise_not(mask)
     return mask
 
